302702046b053d456657dbdb16486f33514da300
Bushman22 <135259957+Bushman22@users.noreply.github.com>
2024-07-14 16:51:00 +0200

    edits

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -19,23 +19,31 @@ function Services() {
                 <Logos />
             </div>
 
-            <div>
-                <div className='text-center mt-28'>
-                    <h1 className='text-4xl font-semibold'>Installer of the year</h1>
-                    <h2 className='text-default-500'>Smartstone Pool Surround</h2>
+            <div className='grid md:grid-cols-2 grid-cols-1 gap-10 mt-20 items-center md:h-[500px]'>
+                <div className='md:h-[500px] flex justify-center'>
+                    <video className="md:w-[400px] w-full h-full object-cover rounded-xl"   controls preload="auto">
+                        <source src="/video.mp4" type="video/mp4" />
+                        Your browser does not support this video
+                    </video>
                 </div>
-                <div className='flex lg:flex-row flex-col items-center justify-center '>
-                    <Link href="https://www.smartstone.co.za/" isExternal>
-                        <Image
-                            radius='0'
-                            removeWrapper
-                            className='w-[300px] h-[300px] max-lg:mt-10 max-lg:w-[150px] max-lg:h-[150px]'
-                            src='/badge.jpeg'
-                            alt=''
-                        />
-                    </Link>
-                    <div className='max-w-xl'>
-                        <p className='text-center text-2xl'>Judges were impressed with the innovative use and neat cutting of the rectangular pavers by Thomson Paving</p>
+                <div>
+                    <div className='text-center'>
+                        <h1 className='text-4xl font-semibold'>Installer of the year</h1>
+                        <h2 className='text-default-500'>Smartstone Pool Surround</h2>
+                    </div>
+                    <div className='grid grid-cols-2 items-center md:gap-5 gap-2 max-lg:mt-10'>
+                        <Link href="https://www.smartstone.co.za/" isExternal>
+                            <Image
+                                radius='0'
+                                removeWrapper
+                                className='w-[300px] h-[300px] max-lg:w-[150px] max-lg:h-[150px]'
+                                src='/badge.jpeg'
+                                alt=''
+                            />
+                        </Link>
+                        <div className=''>
+                            <p className='text-center'>Judges were impressed with the innovative use and neat cutting of the rectangular pavers by Thomson Paving</p>
+                        </div>
                     </div>
                 </div>
             </div>
--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -1,12 +1,14 @@
 "use client";
 
 import React, { useState } from 'react';
-import { Card, CardBody, Input, Button, Textarea } from '@nextui-org/react'
+import emailjs from 'emailjs-com'; // Import emailjs
+
+import { Card, CardBody, Input, Button, Textarea } from '@nextui-org/react';
 
 function ContactForm() {
     const [isLoading, setIsLoading] = useState(false);
     const [isSubmitted, setIsSubmitted] = useState(false);
-
+    const [isError, setIsError] = useState(false);
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -24,17 +26,37 @@ function ContactForm() {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        setIsError(false)
         setIsLoading(true);
-        console.log('Form Data:', formData);
-        await new Promise((resolve) => setTimeout(resolve, 2000));
-        setIsLoading(false);
-        setIsSubmitted(true);
-        setFormData({
-            name: '',
-            email: '',
-            phone: '',
-            message: '',
-        });
+
+        try {
+            const templateParams = {
+                from_name: formData.name,
+                user_email: formData.email,
+                user_phone: formData.phone,
+                message: formData.message,
+            };
+
+            await emailjs.send(
+                'service_xzz07w8',
+                'template_eief64g',
+                templateParams,
+                'XA2NvqhLPZMFZQfnb'
+            );
+
+            setIsSubmitted(true);
+        } catch (error) {
+            setIsError(true);
+            console.error('Error sending email:', error);
+        } finally {
+            setIsLoading(false);
+            setFormData({
+                name: '',
+                email: '',
+                phone: '',
+                message: '',
+            });
+        }
     };
 
     return (
@@ -43,10 +65,10 @@ function ContactForm() {
                 <CardBody className='h-full'>
                     <form className='h-full' onSubmit={handleSubmit}>
                         {isSubmitted ? (
-                            <div className=' flex flex-col items-center justify-center text-center h-full'>
+                            <div className='flex flex-col items-center justify-center text-center h-full'>
                                 <h1 className='font-bold'>Thank you for submitting the form</h1>
-                                <p className='text-default-500'>we will get back to you as soon as we can</p>
-                                <Button className='mt-5' onPress={() => setIsSubmitted(false)} >
+                                <p className='text-default-500'>We will get back to you as soon as we can</p>
+                                <Button className='mt-5' onPress={() => setIsSubmitted(false)}>
                                     Redo form
                                 </Button>
                             </div>
@@ -55,58 +77,62 @@ function ContactForm() {
                                 <div className='flex justify-center text-center'>
                                     <div>
                                         <p className='font-bold mt-2'>Contact Us</p>
-                                        <p className=' mb-3 text-tiny mb-5 text-default-500'>We will get back to you as soon as possible</p>
+                                        <p className='mb-3 text-tiny mb-5 text-default-500'>
+                                            We will get back to you as soon as possible
+                                        </p>
                                     </div>
                                 </div>
                                 <Input
                                     className=''
-                                    type="text"
-                                    name="name"
-                                    label="Name & Surname"
+                                    type='name'
+                                    name='name'
+                                    label='Name & Surname'
                                     value={formData.name}
                                     onChange={handleChange}
                                     required
+                                    autoComplete='name'
                                 />
                                 <Input
                                     className=''
-                                    type="email"
-                                    name="email"
-                                    label="Email"
+                                    type='email'
+                                    name='email'
+                                    label='Email'
                                     value={formData.email}
                                     onChange={handleChange}
                                     required
+                                    autoComplete='email'
                                 />
                                 <Input
                                     className=''
-                                    type="tel"
-                                    name="phone"
-                                    label="Phone"
+                                    type='tel'
+                                    name='phone'
+                                    label='Phone'
                                     value={formData.phone}
                                     onChange={handleChange}
                                     required
                                 />
                                 <Textarea
                                     className=''
-                                    name="message"
+                                    name='message'
                                     value={formData.message}
                                     onChange={handleChange}
                                     placeholder='Message'
                                 />
-                                <Button className='bg-red-500 w-full py-2' size='lg' type='submit' isLoading={isLoading}>
-                                    {isLoading ? (
-                                        <span>Loading</span>
-                                    ) : (
-                                        <span>Submit</span>
-                                    )}
+                                <Button className='bg-red-500 w-full py-2 text-white' size='lg' type='submit' isLoading={isLoading} isError={isError}>
+                                    {isLoading ? <span>Loading</span> : (isError ? <span>Submit Failed</span> : <span>Submit</span>)}
                                 </Button>
+                                {isError && (
+                                    <div className='text-center'>
+                                        <h1 className='text-red-500'>Error submitting form, please try again</h1>
+                                    </div>
+                                )}
                             </div>
                         )}
-
                     </form>
                 </CardBody>
             </Card>
         </div>
-    )
+    );
 }
 
-export default ContactForm
+export default ContactForm;
